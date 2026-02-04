b60c2c134d96df0320f152be8a3f4d821916c553
Cakey01 <alexanderwarren7@gmail.com>
2026-02-04 15:08:31 -0500

    Create hash-map class and set up methods

---
+++ hash-map.js
@@ -0,0 +1,53 @@
+export default class HashMap {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+    }
+    
+    return hashCode;
+  }
+
+  set(key, value) {
+
+  }
+
+  get(key) {
+
+  }
+
+  has(key) {
+
+  }
+
+  remove(key) {
+
+  }
+
+  length() {
+
+  }
+
+  clear() {
+
+  }
+
+  keys() {
+
+  }
+
+  values() {
+
+  }
+
+  entries() {
+    
+  }
+}
